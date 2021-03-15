d5f95a049ffa94d0897a4851f4258835a3f9af89
Gercino Luiz <gercino.neto.luiz@gmail.com>
2021-03-15 11:59:06 -0300

    Remove dist

--- dist/modules/users/providers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _BcryptProvider = _interopRequireDefault(require("./HashProviders/implementations/BcryptProvider"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_tsyringe.container.registerSingleton('HashProvider', _BcryptProvider.default);
--- dist/shared/infra/typeorm/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _typeorm = require("typeorm");
-
-console.log("DataBase Connection STARTED");
-(0, _typeorm.createConnections)();
